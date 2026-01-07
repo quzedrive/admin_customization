cc33a3b0cb6776ac48cbc212fe91231c0dfaa35f
SANJAY D <sanjayjay564@gmail.com>
2026-01-07 18:27:50 +0530

    updated the api

--- client/lib/api/client.ts
+++ client/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
     withCredentials: true, // Important for cookies (refresh token)
     headers: {
         'Content-Type': 'application/json',
