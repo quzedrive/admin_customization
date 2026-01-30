f0b3c78d0dea9ffdc1023d99ec0e7dc1832914a9
SANJAY D <sanjayjay564@gmail.com>
2026-01-30 10:54:32 +0530

    updated the form in repsonive mode aspect ratios

--- client/components/PopupUser.tsx
+++ client/components/PopupUser.tsx
@@ -578,11 +578,11 @@ export default function PopupUser({ onClose, leadData, setLeadData }: ContactMod
                   {loading ? (
                     <>
                       <span className="h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin"></span>
-                      Booking...
+                      <span className='!text-white'>Booking...</span>
                     </>
                   ) : (
                     <>
-                      Let's Go
+                      <span className='!text-white'>Let's Go</span>
                       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5 12H19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M12 5L19 12L12 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
